1a4a8c53dfd96c02f90f6b97e6762778f4e47f62
ItsJustNasty <randomguyplaying@gmail.com>
2017-07-06 13:19:15 +0800

    Update searchuser.js

--- events/searchuser.js
+++ events/searchuser.js
@@ -30,11 +30,3 @@ function findUser(user, msg){
   }
 }
 
-// For detecting if the user have a specific role
-function hasRole(mem, role){
-    if(pluck(mem.roles).includes(role)){
-        return true;
-    } else {
-        return false;
-    }
-}
